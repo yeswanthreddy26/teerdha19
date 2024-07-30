5d321f1f365208cc86da04e21cca772bab8dba90
Praveenchinna14 <162446658+Praveenchinna14@users.noreply.github.com>
2024-07-30 17:34:09 +0530

    Update settings.py

--- Teerdhaproject/settings.py
+++ Teerdhaproject/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 CSRF_TRUSTED_ORIGINS=['https://c8e1-139-167-50-98.ngrok-free.app/']
 
-ALLOWED_HOSTS = ['c8e1-139-167-50-98.ngrok-free.app','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['*','c8e1-139-167-50-98.ngrok-free.app','localhost','127.0.0.1']
 
 
 # Application definition
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'Teerdhaproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME':'booking_jul08',
+        'NAME':'teerdha20',
         'POST':'3306',
-        'PASSWORD':'',
-        'HOST':'localhost',
-        'USER':'root'
+        'PASSWORD':'admin12345',
+        'HOST':'my-rds-instance.ctoc6i4sess4.us-east-2.rds.amazonaws.com',
+        'USER':'praveen'
     }
 }
 
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT=os.path.join(BASE_DIR,"static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
